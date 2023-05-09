d0cca72fb3315f2c73a8a3a4c3b4a67d306dd2e9
Koewkie <darrat199@gmail.com>
2023-05-09 15:14:05 +0200

    Expanded favorites, added list

--- lib/favorites.dart
+++ lib/favorites.dart
@@ -8,10 +8,36 @@ class Favorites extends StatefulWidget {
 }
 
 class _FavoritesState extends State<Favorites> {
+  int count = 2;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Favorites')),
+      body: getFavList(),
+    );
+  }
+
+  ListView getFavList() {
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int position) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: const CircleAvatar(
+              backgroundColor: Colors.deepPurpleAccent,
+            ),
+            title: const Text('Placeholder title'),
+            subtitle: const Text('Placeholder subtitle'),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.delete),
+              color: Colors.grey,
+            ),
+          ),
+        );
+      },
     );
   }
 }
